a20b9ef84db1f6e7f208ec70a8dfe0b3317ce75e
Rolf Timmermans <rolftimmermans@voormedia.com>
2017-10-09 13:53:38 +0200

    Add script.

---
+++ script/build.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -e
+
+if [[ "${npm_config_zmq_external}" == "true" ]]; then
+  echo "Requesting to use external libzmq; skipping build..."
+else
+  export MACOSX_DEPLOYMENT_TARGET=10.9
+  export ZMQ_REPO="libzmq"
+  export ZMQ_VERSION=${ZMQ_VERSION:-"4.2.2"}
+  export ZMQ_PREFIX="${PWD}/libzmq"
+  export ZMQ_SOURCE="https://github.com/zeromq/${ZMQ_REPO}/releases/download/v${ZMQ_VERSION}/zeromq-${ZMQ_VERSION}.tar.gz"
+  export ZMQ_SRC_DIR="zeromq-${ZMQ_VERSION}"
+  export ZMQ_TARBALL="zeromq-${ZMQ_VERSION}.tar.gz"
+
+  mkdir -p "${ZMQ_PREFIX}"
+  cd "${ZMQ_PREFIX}"
+
+  if [ -f "lib/libzmq.a" ]; then
+    echo "Found previously built libzmq; skipping rebuild..."
+  else
+    if [ -f "${ZMQ_TARBALL}" ]; then
+      echo "Found libzmq source; skipping download..."
+    else
+      echo "Downloading libzmq source..."
+      curl "${ZMQ_SOURCE}" -sLo "${ZMQ_TARBALL}" -q
+    fi
+
+    export CFLAGS=-fPIC
+    export CXXFLAGS=-fPIC
+    export PKG_CONFIG_PATH="${ZMQ_PREFIX}/lib/pkgconfig"
+
+    test -d "${ZMQ_SRC_DIR}" || tar xzf "${ZMQ_TARBALL}"
+    cd "${ZMQ_SRC_DIR}"
+
+    echo "Building libzmq..."
+    test -f configure || ./autogen.sh
+    if [[ "${ZMQ_VERSION}" < "4.2.0" ]]; then
+      ./configure "--prefix=${ZMQ_PREFIX}" --with-relaxed --enable-static --disable-shared --without-documentation ${ZMQ_BUILD_OPTIONS}
+    else
+      ./configure "--prefix=${ZMQ_PREFIX}" --disable-pedantic --enable-static --disable-shared --without-docs ${ZMQ_BUILD_OPTIONS}
+    fi
+
+    make -j 2
+    make install
+
+    cd "${ZMQ_PREFIX}"
+    rm -rf "${ZMQ_SRC_DIR}" "${ZMQ_TARBALL}"
+  fi
+
+  cd ".."
+fi
+
+echo "Building libzmq bindings..."
+node-gyp rebuild "$@"
+
+echo "Build complete."
