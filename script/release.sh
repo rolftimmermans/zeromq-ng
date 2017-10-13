ef659ef8577e8d7a58473198d093b6bf0a7b90a9
Rolf Timmermans <rolftimmermans@voormedia.com>
2017-10-13 16:06:36 +0200

    Fix MSYS64 issue on Windows.

--- script/release.sh
+++ script/release.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+
+if [ -n "${WINDIR}" ]; then
+  # Give preference to all MSYS64 binaries. This solves issues with mkdir and
+  # other commands not working properly.
+  export PATH="/usr/bin:${PATH}"
+  export PYTHON="/c/Python27/python"
+fi
+
 detect-libc prebuild --verbose --target 8.6.0 --strip --upload ${GH_TOKEN}
