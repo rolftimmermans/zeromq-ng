2cb60f82adc849b2aa206623eb9a33e37c21ca35
Rolf Timmermans <rolftimmermans@voormedia.com>
2018-03-06 21:01:46 +0100

    Fix API usage.

--- src/inline/hacks.h
+++ src/inline/hacks.h
@@ -4,22 +4,49 @@
 #include "node_api.h"
 #include "node_api_types.h"
 
+#define NAPI_THROW_IF_FAILED(env, status, ...)                                   \
+    if ((status) != napi_ok) {                                                   \
+        Napi::Error::New(env).ThrowAsJavaScriptException();                      \
+        return;                                                                  \
+    }
+
 namespace zmq {
 /* Pending https://github.com/nodejs/node-addon-api/issues/223 */
 struct CallbackScope {
     napi_env env;
-    napi_callback_scope scope;
+    napi_handle_scope handle_scope;
+    napi_callback_scope callback_scope;
 
     CallbackScope(napi_env env) : env(env) {
+        napi_status status;
         napi_async_context context;
-        napi_value resource_name;
-        napi_create_string_utf8(env, "zmq", NAPI_AUTO_LENGTH, &resource_name);
-        napi_async_init(env, nullptr, resource_name, &context);
-        napi_open_callback_scope(env, nullptr, context, &scope);
+        napi_value resource_name, resource_object;
+
+        status = napi_open_handle_scope(env, &handle_scope);
+        NAPI_THROW_IF_FAILED(env, status);
+
+        status =
+            napi_create_string_utf8(env, "zmq", NAPI_AUTO_LENGTH, &resource_name);
+        NAPI_THROW_IF_FAILED(env, status);
+
+        status = napi_create_object(env, &resource_object);
+        NAPI_THROW_IF_FAILED(env, status);
+
+        status = napi_async_init(env, resource_object, resource_name, &context);
+        NAPI_THROW_IF_FAILED(env, status);
+
+        status = napi_open_callback_scope(
+            env, resource_object, context, &callback_scope);
+        NAPI_THROW_IF_FAILED(env, status);
     }
 
     ~CallbackScope() {
-        napi_close_callback_scope(env, scope);
+        napi_status status;
+        status = napi_close_callback_scope(env, callback_scope);
+        NAPI_THROW_IF_FAILED(env, status);
+
+        status = napi_close_handle_scope(env, handle_scope);
+        NAPI_THROW_IF_FAILED(env, status);
     }
 };
 }
