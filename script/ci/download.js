4acd4a4bacba30532d4766ed23eb69a877657fb1
Rolf Timmermans <rolftimmermans@voormedia.com>
2019-09-29 19:12:36 +0200

    Test version equality.

--- script/ci/download.js
+++ script/ci/download.js
@@ -5,6 +5,11 @@ const tar = require("tar-fs")
 
 async function download() {
   const {repository: {url}, version} = require(path.resolve("./package.json"))
+
+  if (process.env.TRAVIS_TAG && process.env.TRAVIS_TAG != version) {
+    throw new Error(`Version mismatch (TRAVIS_TAG=${process.env.TRAVIS_TAG}, version=${version}`)
+  }
+
   const [, user, repo] = url.match(/\/([a-z0-9_-]+)\/([a-z0-9_-]+)\.git$/i)
 
   const res = await fetch(`https://api.github.com/repos/${user}/${repo}/releases/tags/${version}`)
