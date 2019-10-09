2ceac669e649b24634946ff4b99ebe4678dcd052
Rolf Timmermans <rolftimmermans@voormedia.com>
2019-10-09 09:06:42 +0200

    Load compat layer from TS src.

--- test/unit/compat/load.js
+++ test/unit/compat/load.js
@@ -3,8 +3,8 @@ const path = require("path")
 module.exports = require(
   process.env.ZMQ_COMPAT_PATH ?
   path.resolve(process.cwd(), process.env.ZMQ_COMPAT_PATH) :
-  "../../../compat"
+  "../../../src/compat"
 )
 
 /* Copy capabilities from regular module. */
-module.exports.capability = require("../../..").capability
+module.exports.capability = require("../../../src").capability
