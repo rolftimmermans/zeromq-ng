503cc0bc7d290c8e858581c78b992618b624ff4b
Rolf Timmermans <rolftimmermans@voormedia.com>
2019-10-10 15:39:49 +0200

    Remove timeout, because it does not kill the test process, making the retried test run more likely to fail.

--- script/ci/test.sh
+++ script/ci/test.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 set -e
 
-if [ -x "$(command -v gtimeout)" ]; then
-  # On homebrew timeout is aliased as gtimeout.
-  alias timeout=gtimeout
-fi
-
-# Tests can sometimes hang in CI environments. To ensure this does not cause
-# very long timeouts (10 minutes on Travis, 60 on AppVeyor), we're going to
-# enforce a 60 second timeout ourselves. If that fails it should be retried.
 if [ -n "${ALPINE_CHROOT}" ]; then
-  /alpine/enter-chroot timeout 60 yarn dev:test
+  /alpine/enter-chroot yarn dev:test
 else
-  timeout 60 yarn dev:test
+  yarn dev:test
 fi
